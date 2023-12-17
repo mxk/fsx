1c8d45a182fb9fabe6193f18c1b33823a57629b4
Maxim Khitrov <max@mxcrypt.com>
2023-12-17 11:46:19 -0500

    Add Path tests

--- index/path.go
+++ index/path.go
@@ -7,33 +7,47 @@ import (
 	"strings"
 )
 
-// Path is a non-empty, unrooted, slash-separated path. Except for the special
-// "." root, the path ends in a '/' if it refers to a directory.
+// Path is a non-empty, unrooted, clean, slash-separated path. Except for the
+// special "." root, the path ends in a '/' if it refers to a directory.
 type Path struct{ p string }
 
 // Root is the special "." path.
 var Root = Path{"."}
 
+// filePath wraps a path to a file. It panics if the path is not a file, not
+// slash-separated, or not clean.
 func filePath(p string) Path {
 	if path.Clean(filepath.ToSlash(p)) != p {
-		panic(fmt.Sprintf("index: non-clean file path: %q", p))
+		panic(fmt.Sprintf("index: non-clean or non-file path: %s", p))
+	}
+	if p == "." {
+		panic("index: non-file path: .")
 	}
 	return Path{p}
 }
 
+// String returns the raw path.
 func (p Path) String() string {
 	return p.p
 }
 
+// IsDir returns whether the path refers to a directory.
 func (p Path) IsDir() bool {
-	return p.p == "." || p.p[len(p.p)-1] == '/'
+	return p.p == "." || (0 < len(p.p) && p.p[len(p.p)-1] == '/')
+}
+
+// Contains returns whether other is under the directory tree p. It returns true
+// if the paths are equal or if p is ".".
+func (p Path) Contains(other Path) bool {
+	return p.p == "." || (0 < len(p.p) && len(p.p) <= len(other.p) &&
+		other.p[:len(p.p)] == p.p && p.p[len(p.p)-1] == '/')
 }
 
 // Dir returns the parent directory of p.
 func (p Path) Dir() Path {
-	switch i := strings.LastIndexByte(p.p, '/'); i {
+	switch i := strings.LastIndexByte(p.p[:len(p.p)-1], '/'); i {
 	case -1:
-		return Path{"."}
+		return Root
 	case 0:
 		panic(fmt.Sprintf("index: rooted path: %s", p))
 	default:
@@ -46,10 +60,6 @@ func (p Path) Base() string {
 	return path.Base(p.p)
 }
 
-func (p Path) Contains(other Path) bool {
-	return p.p == "." || (strings.HasPrefix(other.p, p.p) && p.p[len(p.p)-1] == '/')
-}
-
 // less returns whether path p should be sorted before other. Directories are
 // sorted before files.
 func (p Path) less(other Path) bool {
@@ -59,28 +69,29 @@ func (p Path) less(other Path) bool {
 	}
 	// Find the first byte mismatch
 	for i := 0; i < len(a) && i < len(b); i++ {
-		if ca, cb := a[i], b[i]; ca != cb {
+		if a[i] != b[i] {
 			// Directory is less than a file
-			if aDir := isDir(a[i:]); aDir != isDir(b[i:]) {
+			aDir := strings.IndexByte(a[i:], '/') >= 0
+			if aDir != (strings.IndexByte(b[i:], '/') >= 0) {
 				return aDir
 			}
 			// Path separator is less than any other byte
-			return ca == '/' || (cb != '/' && ca < cb)
+			return a[i] == '/' || (a[i] < b[i] && b[i] != '/')
 		}
 	}
 	// One of the paths is a prefix of the other. If needed, swap the paths so
-	// that a is a prefix of b to simplify the logic.
+	// that a is a prefix of b to simplify the remaining logic.
 	invert := false
 	if len(a) >= len(b) {
 		if len(a) == len(b) {
-			return false // Paths are identical
+			return false // Same path
 		}
 		a, b = b, a
 		invert = true
 	}
 	// a is a prefix of b and the next byte in b cannot be a '/'. We require
 	// directories to end with a '/' to ensure consistent ordering when sorting
-	// ["b/", "b/c", "a"]. Without the terminal '/', we'd sort "a" before "b"
+	// ["b/", "b/c", "a"]. Without the '/' suffix, we'd sort "a" before "b"
 	// since we wouldn't know that "b" is a directory.
 	bSep := strings.IndexByte(b[len(a):], '/')
 	if bSep == 0 {
@@ -90,8 +101,4 @@ func (p Path) less(other Path) bool {
 	// separators, then a and b are regular files in the same directory and a is
 	// shorter. Otherwise, a is a file and b is a directory.
 	return (a[len(a)-1] == '/' || bSep < 0) != invert
-}
-
-func isDir(s string) bool {
-	return strings.IndexByte(s, '/') >= 0
 }
--- index/path_test.go
+++ index/path_test.go
@@ -6,6 +6,52 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFilePath(t *testing.T) {
+	assert.Equal(t, "a", filePath("a").String())
+	assert.Equal(t, "a/b", filePath("a/b").String())
+	assert.Panics(t, func() { filePath("") })
+	assert.Panics(t, func() { filePath(".") })
+	assert.Panics(t, func() { filePath("a/") })
+}
+
+func TestPathIsDir(t *testing.T) {
+	assert.True(t, Root.IsDir())
+	assert.False(t, Path{"a"}.IsDir())
+	assert.True(t, Path{"a/"}.IsDir())
+}
+
+func TestPathContains(t *testing.T) {
+	assert.True(t, Root.Contains(Root))
+	assert.True(t, Root.Contains(Path{"a"}))
+	assert.True(t, Root.Contains(Path{"a/"}))
+	assert.False(t, Path{"a"}.Contains(Path{"a"}))
+	assert.False(t, Path{"a/a"}.Contains(Path{"a/a"}))
+	assert.False(t, Path{"a/"}.Contains(Root))
+	assert.False(t, Path{"a/"}.Contains(Path{"a"}))
+	assert.False(t, Path{"a/"}.Contains(Path{"b"}))
+	assert.False(t, Path{"a/"}.Contains(Path{"b/"}))
+	assert.False(t, Path{"a/b"}.Contains(Path{"a/"}))
+	assert.True(t, Path{"a/"}.Contains(Path{"a/"}))
+	assert.True(t, Path{"a/"}.Contains(Path{"a/b"}))
+}
+
+func TestPathDirBase(t *testing.T) {
+	tests := []struct{p, dir, base string}{
+		{".", ".", "."},
+		{"a", ".", "a"},
+		{"a/", ".", "a"},
+		{"a/b", "a/", "b"},
+		{"a/b/", "a/", "b"},
+		{"a/bc/de", "a/bc/", "de"},
+		{"a/bc/de/", "a/bc/", "de"},
+	}
+	for _, tc := range tests {
+		assert.Equal(t, Path{tc.dir}, Path{tc.p}.Dir(), "%q", tc)
+		assert.Equal(t, tc.base, Path{tc.p}.Base(), "%q", tc)
+	}
+	assert.Panics(t, func() { Path{"/a"}.Dir() })
+}
+
 func TestPathLess(t *testing.T) {
 	less := []struct{ a, b string }{
 		{".", "!"},
@@ -17,7 +63,9 @@ func TestPathLess(t *testing.T) {
 		{"b/c", "a"},
 		{"b/", "a"},
 
+		{"b/", "ba"},
 		{"b/c", "ba"},
+		{"b/c", "ba/"},
 		{"b/c", "b/ca"},
 		{"a/b", "aa/b"},
 		{"aa/b", "a"},
@@ -31,6 +79,7 @@ func TestPathLess(t *testing.T) {
 	panics := []struct{ a, b string }{
 		{"a/", "a"},
 		{"a/a", "a"},
+		{"a/b/c", "a/b"},
 	}
 	for _, tc := range panics {
 		assert.Panics(t, func() { Path{tc.a}.less(Path{tc.b}) }, "%q", tc)
@@ -38,4 +87,6 @@ func TestPathLess(t *testing.T) {
 	}
 	assert.False(t, Root.less(Root))
 	assert.False(t, Path{"a"}.less(Path{"a"}))
+	assert.False(t, Path{"a/"}.less(Path{"a/"}))
+	assert.False(t, Path{"a/b"}.less(Path{"a/b"}))
 }
